368be6d04d4ae69790185389b90d36ca0206c72c
Shinonononon <h1karu.sn0928@gmail.com>
2024-07-10 17:37:45 +0900

    aaa

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,13 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: '登録完了')
+    @name = params[:name]
+    mail(to:  params[:to], subject: '登録完了')
   end
 end
+
+# def welcome
+#   @name = params[:name] # params: 呼び出し元から渡されたデータ
+#   # インスタンス変数に代入すると次のビューで使える
+#   mail(to: params[:to], subject: "登録完了") # メール送信
+# end
